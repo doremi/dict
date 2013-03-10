d0075bf67154ba992d21f42da9e1f35367f699df
doremi <doremi@vmware>
2013-03-10 10:46:18 +0800

    Translate the result from Simplfied-Chinese to Tranditional-Chinese

--- lookup.sh
+++ lookup.sh
@@ -2,6 +2,6 @@
 
 while [ 1 ]; do
     read -p "Words: " words
-    dict -d xdict $words
+    dict -d xdict $words | iconv -f utf8 -t gb2312 | iconv -f gb2312 -t big5 | iconv -f big5 -t utf8
     echo			# print new line
 done
